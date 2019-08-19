08fe87d58b988e6e9989a6b90f19dd45785b162a
matthew-williams1 <51970965+matthew-williams1@users.noreply.github.com>
2019-08-18 21:44:50 -0400

    Update and rename Ising model.py to Ising_model.py

--- Ising_model.py
+++ Ising_model.py
@@ -1,20 +1,21 @@
-from LatticeClass import Lattice
+from LatticeClass import Lattice # Imports the Lattice class
 import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.animation as anim
-import h5py
+import h5py # Imports the library used for storing data
 
 file = h5py.File("Lattice.hdf5", "w")  # Initialize a file object to save the cooling history of the lattice.
 
-a = 0
-width = 40
-height = 40
-temperature = 12  # Temperature is in Kelvin
+a = 0             # Relative strength of diagonal particles on a given particle
+width = 40        # Initializing width of lattice
+height = 40       # Initializing length of lattice
+temperature = 12  # Starting temperature for simulation (in Kelvin)
 
+# Initializing the animation plot
 fig = plt.figure()
 fig.clear()
 
-
+# Performs one Monte Carlo step and visualizes the result while lowering the temp
 def animate(i):
     fig.clear()
     lattice.monte_carlo(1)
@@ -22,21 +23,25 @@ def animate(i):
     fig.suptitle(temperature_string)
     lattice.visualize(filtered=True)
 
-
+# Initializing lattice variable
 lattice = Lattice(width, height, temperature)
 
+# Initializing animation variable
 anim = anim.FuncAnimation(fig, animate)
 plt.show()
 
+# Visualizing magnetic field
 plt.imshow(lattice.get_magnetic_field(), cmap='winter', interpolation='None')
 plt.show()
+
 
 plt.imshow((lattice.get_running_sum() - np.amin(lattice.get_running_sum())) /
            (np.amax(lattice.get_running_sum()) - np.amin(lattice.get_running_sum())),
            cmap='winter', interpolation='None')
+
 print(lattice.mse())
 plt.show()
 
-
+# Initializes files to store the training data and the labels for the ANN
 # train_data = file.create_dataset("train_data", data=np.packbits(lattice.getCoolingHistory(), axis=0))
 # labels = file.create_dataset("labels", data=np.packbits(lattice.getMagneticField(), axis=0))
